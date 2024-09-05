6daf05b12cfc358678bf9e6f0fd3f98b48f3c376
Lyubomir Yordanov <143926613+lyubomir08@users.noreply.github.com>
2024-09-05 18:34:47 +0300

    Add validation when attaching a cast

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -29,21 +29,37 @@ exports.create = (movieData) => Movie.create(movieData);
 exports.edit = (movieId, movieData) => Movie.findByIdAndUpdate(movieId, movieData);
 
 exports.attach = async (movieId, castId) => {
-    // return Movie.findByIdAndUpdate(movieId, { $push: { casts: castId } });
     const movie = await this.getOne(movieId);
+    const cast = await Cast.findById(castId);
+
+    // Validate if cast exists
+    if (!cast) {
+        throw new Error(`Cast with Id ${castId} does not exist`);
+    }
+
+    // Validate if cast is already added
+    if (movie.casts.includes(castId)) {
+        throw new Error(`Cast with Id ${castId} is already added to the movie`);
+    }
+
+    // Add cast to movie
+    movie.casts.push(castId);
+    await movie.save();
+
+    return movie;
+
+    // return Movie.findByIdAndUpdate(movieId, { $push: { casts: castId } });
+
+    // const movie = await this.getOne(movieId);
+    // movie.casts.push(cast);
+    // await movie.save();
 
     // This is optional and we don't need it in this case
     // const cast = await Cast.findById(castId);
     // cast.movies.push(movie);
     // await cast.save();
 
-    // TODO: validate castId if exists
-    // TODO: validate if cast is already added
-    movie.casts.push(cast);
-
-    await movie.save();
-
-    return movie;
+    // return movie;
 };
 
 exports.delete = (movieId) => Movie.findByIdAndDelete(movieId);
